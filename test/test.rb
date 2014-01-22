c2bc7ba88fd74c3c4bff8bbd0b0077964a8f076e
Gabrielle Epstein <doctorgabi@gmail.com>
2014-01-22 10:49:09 -0600

    Correctly names minitests.

--- test/test.rb
+++ test/test.rb
@@ -2,55 +2,55 @@ require_relative 'helper'
 
 class TestEnteringWords <MiniTest::Unit::TestCase
 
- def test0_error_message_for_missing_information
+ def test_0_error_message_for_missing_all_arguments
   command = "./jstudy add"
   expected = "You must provide the kunyomi and english and jlpt level and category of the word you are adding."
   assert_command_output expected, command
  end
 
- def test01_error_message_for_missing_kunyomi
+ def test_01_error_message_for_missing_one_argument_kunyomi
   command = "./jstudy add --english occasionally --jlptlevel 0 --category selfstudy"
   expected = "You must provide the kunyomi of the word you are adding."
   assert_command_output expected, command
  end
 
- def test02_error_message_for_missing_english
+ def test_02_error_message_for_missing_one_argument_english
   command = "./jstudy add --kunyomi ときどき --jlptlevel 0 --category selfstudy"
   expected = "You must provide the english of the word you are adding."
   assert_command_output expected, command
  end
 
- def test03_error_message_for_missing_JLPTlevel
+ def test_03_error_message_for_missing_one_argument_JLPTlevel
   command = "./jstudy add --kunyomi ときどき --english occasionally --category selfstudy"
   expected = "You must provide the jlpt level of the word you are adding."
   assert_command_output expected, command
  end
 
- def test04_error_message_for_missing_category
+ def test_04_error_message_for_missing_one_argument_category
   command = "./jstudy add --kunyomi ときどき --english occasionally --jlptlevel 0"
   expected = "You must provide the category of the word you are adding."
   assert_command_output expected, command
  end
 
- def test05_error_message_for_missing_kunyomi_and_category
+ def test_05_error_message_for_missing_two_arguments_kunyomi_and_category
   command = "./jstudy add --english occasionally --jlptlevel 0"
   expected = "You must provide the kunyomi and category of the word you are adding."
   assert_command_output expected, command
  end
 
- def test06_error_message_for_missing_english_and_jlptlevel
+ def test_06_error_message_for_missing_two_arguments_english_and_jlptlevel
   command = "./jstudy add --kunyomi ときどき --category selfstudy"
   expected = "You must provide the english and jlpt level of the word you are adding."
   assert_command_output expected, command
  end
 
- def test07_error_message_for_missing_english_and_jlptlevel_and_category
+ def test_07_error_message_for_missing_three_arguments_english_and_jlptlevel_and_category
   command = "./jstudy add --kunyomi ときどき"
   expected = "You must provide the english and jlpt level and category of the word you are adding."
   assert_command_output expected, command
  end
 
- def test05_that_correct_info_is_accepted
+ def test_08_that_all_correct_info_is_accepted
   command = "./jstudy add --kunyomi ときどき --english occasionally --JLPTlevel 0 --category selfstudy"
   expected = "Theoretically creating: a word with the kunyomi ときどき, the english translation 'occasionally', JLPT level 0 and category 'selfstudy'"
   assert_command_output expected, command
