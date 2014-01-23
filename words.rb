6d303dddb02ecb37beb842772987becc18c1fb0e
Gabrielle Epstein <doctorgabi@gmail.com>
2014-01-23 17:26:32 -0600

    Removes superfluous spaces and comments.

--- words.rb
+++ words.rb
@@ -10,28 +10,20 @@ class Words
   "A word with the kunyomi #{options[:kunyomi]}, the english translation '#{options[:english]}', JLPT level #{options[:jlptlevel]} and category '#{options[:category]}' was created."
  end
 
- def update
+ def self.search(input)
+  puts "You asked for: #{input}"
+ end
 
+ def update
  end
 
  def delete
-
- end
-
- def self.search
-
  end
 
  def self.all
-
  end
 
  def to_s
-
  end
+
 end
-
-
-# You only have one model at this point, so this is a great time to pull all of that information into one class.
-
-# Today, try to take everything related to your first model and pull it into a class.
