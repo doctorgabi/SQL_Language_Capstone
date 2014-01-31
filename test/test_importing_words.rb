dbe26a689887cf9b6c6fd1b55ba6fa2570af3551
Gabrielle Epstein <doctorgabi@gmail.com>
2014-01-30 21:42:25 -0600

    WIP: Failing tests for importer.

--- test/test_importing_words.rb
+++ test/test_importing_words.rb
@@ -25,4 +25,21 @@ require_relative '../lib/importer'
    assert_equal expected, actual
   end
 
+  def test_03_extra_skills_arent_created
+   import_data
+   assert 3, Skill.all.count
+  end
+
+  def test_04_skills_are_created_as_needed
+   Skill.create("know")
+   Skill.create("review")
+   import_data
+   assert 3, Skill.all.count
+  end
+
+  def test_05_data_isnt_duplicated
+   import_data
+   expected = ["know", "review", "unknown"]
+   assert_equal expected, Skill.all.map(&:name)
+  end
  end
